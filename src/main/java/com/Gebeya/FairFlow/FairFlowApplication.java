ff4dca39bd38d79228990fd9fa7ae96afa35a460
Noah Hailegiorgis <noahhaile456@gmail.com>
2024-01-24 06:16:39 +0300

    first commit

---
+++ src/main/java/com/Gebeya/FairFlow/FairFlowApplication.java
@@ -0,0 +1,13 @@
+package com.Gebeya.FairFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Gebeya/FairFlow/FairFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gebeya.FairFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
